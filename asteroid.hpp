8709beccec080c5d73be06a6d9110b08be9f2784
Illy <93146324+illy-olga@users.noreply.github.com>
2023-11-17 13:59:37 +0100

    Create asteroid.hpp

---
+++ asteroid.hpp
@@ -0,0 +1,47 @@
+#ifndef Asteroid_HPP
+#define Asteroid_HPP
+#include <iostream>
+#include "FlyingObject.hpp"
+#include <string>
+
+using namespace std;
+
+class Asteroid : public FlyingObject
+{
+private:
+    double m_x;
+    double m_y;
+    double m_size;
+    double m_xSpeed;
+    double m_ySpeed;
+
+public :
+    Asteroid(double x, double y, double size, double xSpeed, double ySpeed);
+
+    
+    double getXSpeed();
+    double getYSpeed();
+    
+
+    void setX(double x);
+    void setY(double y);
+    void setXSpeed(double xSpeed);
+    void setYSpeed(double ySpeed);
+    void setSize(double size);
+
+    void move(double screenWidth, double screenHeight);
+
+    void move();
+
+    std::string GetTypeName() const override {
+        return "Asteroid";
+    }
+        
+};  
+
+#endif
+
+
+
+
+
