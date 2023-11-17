7a08f7d233b66b4400ba00d3a53684f4b523a4cb
Illy <93146324+illy-olga@users.noreply.github.com>
2023-11-17 14:04:59 +0100

    Create view.hpp

---
+++ view.hpp
@@ -0,0 +1,23 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <vector>
+#include "FlyingObject.hpp"
+#include "Model.hpp"
+#include "framework.hpp"
+
+class View {
+private:
+    SDL_Texture* textureShip;
+    SDL_Texture* textureAsteroid;
+    SDL_Texture* textureMissile;
+
+    //std::vector<Sprite> sprites;
+public:
+    View();
+    ~View();
+    void Actualise_Affichage(const std::vector<FlyingObject *>& gameObjects, Framework* framework);
+    
+};
+
+#endif
