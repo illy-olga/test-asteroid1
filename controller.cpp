16538aaffc0e8cbcaebe459f4fc42ea9e8b75c2b
Illy <93146324+illy-olga@users.noreply.github.com>
2023-11-17 14:00:16 +0100

    Create controller.cpp

---
+++ controller.cpp
@@ -0,0 +1,34 @@
+#include "Controller.hpp"
+#include "View.hpp"
+#include "Model.hpp"
+#include "framework.hpp"
+
+
+
+Controller::Controller(int fps, int shipSize, int missileSize){
+    framework = new Framework(fps, shipSize,missileSize);
+    model = new Model(framework);
+    view = new View();
+}
+
+Controller::~Controller(){
+    delete model;
+    delete view;
+    delete framework;
+}
+
+void Controller::lauchGame(){
+    while(true){
+        int action = framework->GetInput();
+
+        model->UpdateAction(action);
+        model->UpdateData(framework);
+
+        std::vector<FlyingObject*> gameObjects = model->getFlyingObjects();
+        view->Actualise_Affichage(gameObjects, framework);
+
+
+
+    }
+}
+
