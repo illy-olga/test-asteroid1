b0babb3adacf604a0540a4784bbcab243dfe3926
Illy <93146324+illy-olga@users.noreply.github.com>
2023-11-19 00:42:53 +0100

    Update model.hpp

--- model.hpp
+++ model.hpp
@@ -7,6 +7,7 @@
 #include "Asteroid.hpp"
 #include "missile.hpp"
 #include "framework.hpp"
+#include <random>
 
 class Model {
 private:
@@ -14,23 +15,29 @@ private:
     Missile* missile;
     Asteroid* asteroid;
     std::vector<FlyingObject*> flyingObjects;
+    std::uniform_int_distribution<int> angleDist;
+    //void IniatialisationAsteroid();
+
+
+
 public:
     Model(Framework* framework);
     ~Model();
 
     void UpdateAction(int action);
-    
     void UpdateData(Framework* framework);
+
     std::vector<FlyingObject *> getFlyingObjects() const;
+    void moveShipLeft();
+    void moveShipRight();
+    void fireMissile();
+    void update();
     std::vector<FlyingObject*> GetGameObjects() const;
     
     void setScreenSize(int screenWidth, int screenHeight);
 
     //Spaceship& getSpaceship() const { return *spaceship;}
-    void moveShipLeft();
-    void moveShipRight();
-    void fireMissile();
-    void update();
+    
 
     Spaceship* getSpaceship() const {
         return spaceship;
