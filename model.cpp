7fcfc378a41e41dc56b0fca205ff8df80025bd6f
Illy <93146324+illy-olga@users.noreply.github.com>
2023-11-19 00:42:33 +0100

    Update model.cpp

--- model.cpp
+++ model.cpp
@@ -5,15 +5,101 @@
 #include "Model.hpp"
 #include "framework.hpp"
 #include <string>
+#include <random>
 
-Model::Model(Framework* framework){
-    spaceship = new Spaceship(framework->GetScreenWidth()/2,framework->GetScreenHeight()/2,20,0,0);
-    missile = new Missile(0,0,5,3,30);
-    asteroid = new Asteroid(100,100,50,10,15);
-    flyingObjects = std::vector<FlyingObject*>();
-    flyingObjects.push_back(spaceship);
-    flyingObjects.push_back(asteroid);
-    flyingObjects.push_back(missile);
+
+Model::Model(Framework* framework) : angleDist(-180,180) {
+
+    flyingObjects.push_back(new Spaceship(framework->GetScreenWidth()/2,framework->GetScreenHeight()/2,20,0,0));
+    flyingObjects.push_back(new Missile(200,200,500,10,80));
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> zoneDist(0,7);
+
+    int nb_asteroids = 10;
+    for (int i =0; i<nb_asteroids; i++){
+        int selectedZone = zoneDist(gen);
+
+        int minX, minY, maxX, maxY;
+
+
+
+        switch (selectedZone) {
+            case 0:
+                minX = 0;
+                minY = 0;
+                maxX = framework->GetScreenWidth() /3;
+                maxY= framework->GetScreenHeight() /3;
+                break;
+
+            case 1:
+                minX = framework->GetScreenWidth() /3;
+                minY = 0;
+                maxX = 2 * (framework->GetScreenWidth() /3);
+                maxY= framework->GetScreenHeight() /3;
+                break;
+
+            case 2:
+                minX = 2 * (framework->GetScreenWidth() /3);
+                minY = 0;
+                maxX = framework->GetScreenWidth();
+                maxY= framework->GetScreenHeight() /3;
+                break;
+
+            case 3:
+                minX = 0;
+                minY = framework->GetScreenHeight() /3;
+                maxX = framework->GetScreenWidth() /3;
+                maxY= 2 * (framework->GetScreenHeight() /3);
+                break;
+
+            case 4:
+                minX = framework->GetScreenWidth() /3;
+                minY = framework->GetScreenHeight() /3;
+                maxX = 2 * (framework->GetScreenWidth() /3);
+                maxY= 2 * (framework->GetScreenHeight() /3);
+                break;
+
+            case 5:
+                minX = 2 * (framework->GetScreenWidth() /3);
+                minY = framework->GetScreenHeight() /3;
+                maxX = framework->GetScreenWidth() /3;
+                maxY= 2 * (framework->GetScreenHeight() /3);
+                break;
+
+            case 6:
+                minX = 0;
+                minY = 2 * (framework->GetScreenHeight() /3);
+                maxX = framework->GetScreenWidth() /3;
+                maxY= framework->GetScreenHeight() /3;
+                break;
+
+            default:
+                break;
+
+        }
+
+        std::uniform_real_distribution<double> coordDistX(minX,maxX);
+        std::uniform_real_distribution<double> coordDistY(minY,maxY);
+
+        std::uniform_real_distribution<double> randomSize(50,200);
+        double random_size = randomSize(gen);
+
+        double randomAngle = static_cast<double>(angleDist(gen));
+        
+        flyingObjects.push_back(new Asteroid(coordDistX(gen), coordDistY(gen), random_size,10, randomAngle));
+
+    }
+
+   
+
+
+
+    //flyingObjects = std::vector<FlyingObject*>();
+   
+
+    //IniatialisationAsteroid();
 
 }
 
@@ -82,6 +168,111 @@ std::vector<FlyingObject*> Model::GetGameObjects() const {
     return flyingObjects;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*void Model::generateRandomAsteroidPosition(){
+    std::default_random_engine generator(static_cast<unsigned>(time(0)));
+    std::uniform_int_distribution<int> zoneDistribution(0,7);
+    int selectedZone = zoneDistribution(generator);
+
+    int minX, maxX,minY,maxY;
+    switch(selectedZone) {
+        case 0:
+            minX = 0;
+            maxX = framework->GetScreenWidth()/3;
+            minY = 0;
+            maxY = framework->GetScreenHeight()/2;
+            break;
+
+    
+    }
+
+    std::uniform_real_distribution<double> xDistribution(minX, maxX);
+    std::uniform_real_distribution<double> yDistribution(minY, maxY);
+
+    asteroid->setX(xDistribution(generator));
+    asteroid->setY(yDistribution(generator));
+
+double Model::generateRandomAngle() {
+    std::default_random_engine generator(static_cast<unsigned>(time(0)));
+    std::uniform_real_distribution<double> angleDistribution(-180.0, 180.0);
+    return angleDistribution(generator);
+} */
+
+
+
+
+
+
+
+
+/*void Model::IniatialisationAsteroid() {
+    std::mt19937 rng(std::random_device{}());
+
+    std::uniform_int_distribution<int> zoneDistribution(0,7);
+    int selectedZone = zoneDistribution(rng);
+
+
+
+    int minX = (selectedZone % 3) * (framework->GetScreenWidth()/3);
+    int maxX = minX + (framework->GetScreenWidth() / 3);
+    int minY = (selectedZone / 3) * (framework->GetScreenHeight()/3);
+    int maxY = minY + (framework->GetScreenHeight() / 3);
+    
+    std::uniform_real_distribution<double> xDistribution(minX,maxX);
+    std::uniform_real_distribution<double> yDistribution(minY,maxY);
+
+    double randomX = xDistribution(rng);
+    double randomY = yDistribution(rng);
+    
+    asteroid->setX(randomX);
+    asteroid->setY(randomY);
+
+    std::uniform_real_distribution<double> angleDistribution(-180,180);
+    double randomAngle = angleDistribution(rng);
+
+    double xSpeed = asteroid->getSpeed() * sin(M_PI * randomAngle /180);
+    double ySpeed = asteroid->getSpeed() * cos(M_PI * randomAngle /180);
+
+    //Asteroid* asteroid = new Asteroid();
+    asteroid->setSpeed(xSpeed);
+    asteroid->setSpeed(ySpeed);
+
+}*/
+
+
+/*
+void Model::CreateRandomAsteroid() {
+
+    std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+
+    std::uniform_int_distribution<int> zoneDistribution(0,7);
+    int selectedZone = zoneDistribution(generator);
+
+    double minX, maxX, minY, maxY;
+
+    std::uniform_real_distribution<double> coordDistributionX(minX,maxX);
+    std::uniform_int_distribution<double> coordDistributionY(minY,maxY);
+
+    double asteroidX = coordDistributionX(generator);
+    double asteroidY = coordDistributionY(generator);
+
+
+}*/
 
 
 /*Model model(screenWidth, screenHeight);
