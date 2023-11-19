2ede87808267a31e5a44149ca6bc36d7a267bf78
Illy <93146324+illy-olga@users.noreply.github.com>
2023-11-19 20:12:57 +0100

    Update spaceship.hpp

--- spaceship.hpp
+++ spaceship.hpp
@@ -1,7 +1,8 @@
 #ifndef SPACESHIP_HPP
 #define SPACESHIP_HPP
 #include <string>
-#include "FlyingObject.hpp"
+#include "flyingObject.hpp"
+#include <chrono>
 
 class Spaceship : public FlyingObject  {
 private :
@@ -10,31 +11,28 @@ private :
     double m_Xspeed;
     double m_Yspeed;
 
+    double m_shieldLevel;
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_invincibleEndTime;
+
+
 public:
     Spaceship(double x, double y, double size, double speed,double angle);
     
+    double GetShieldLevel() const;
+    void SetShieldLevel(double level);
+    bool GetInvincible() const;
+    void SetInvincibleFor(double duration);
     double GetAngle();
-
     void SpeedUp(double acceleration);
     void SpeedDown(double deceleration);
     void Rotate(double r_Angle);
-
-    //std::string GetTypeName() override;
-
     float getShieldLevel() const;
     bool isWarning() const;
-
     void move(double screenWidth, double screenHeight) override;
 
     std::string GetTypeName() const override {
         return "Spaceship";
     }
-
-
-
 };
-
-
-
 
 #endif
