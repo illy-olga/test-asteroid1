f2aa6893a71fd512b509b1dadd9d74cd4c1ed0c2
Illy <93146324+illy-olga@users.noreply.github.com>
2023-11-19 20:09:54 +0100

    Update controller.hpp

--- controller.hpp
+++ controller.hpp
@@ -1,9 +1,9 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#include "Model.hpp"
-#include "View.hpp"
-#include "Framework.hpp"
+#include "model.hpp"
+#include "view.hpp"
+#include "framework.hpp"
 
 class Controller {
 private:
@@ -12,13 +12,12 @@ private:
     Framework* framework ; /*à réfléchir !! Attention mettre des pointeurs !*/
 
 public:
-    //Controller(Model m,View* v, Framework* f);
+    
     Controller(int fps, int shipSize, int missileSize);
     ~Controller();
     Controller(Framework* fk);
 
     void lauchGame();
-    //void updateView();
 
     void setScreenSize(int screenWidth, int screenHeight);
 
